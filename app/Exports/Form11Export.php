1cdb70214d054b06ad5a3d4da173df9d419235c3
MuazamIftikhar <51464624+MuazamIftikhar@users.noreply.github.com>
2020-08-07 18:41:17 +0500

    yes.png

--- app/Exports/Form11Export.php
+++ app/Exports/Form11Export.php
@@ -28,14 +28,14 @@ class Form11Export implements FromView,WithEvents,WithDrawings
         $drawing = new Drawing();
         $drawing->setName('Logo');
         $drawing->setDescription('This is my logo');
-        $drawing->setPath(public_path('images\yes.png'));
+        $drawing->setPath(public_path('images/yes.png'));
         $drawing->setHeight(20);
         $drawing->setCoordinates('F13');
 
         $drawing2 = new Drawing();
         $drawing2->setName('Other image');
         $drawing2->setDescription('This is a second image');
-        $drawing2->setPath(public_path('images\yes.png'));
+        $drawing2->setPath(public_path('images/yes.png'));
         $drawing2->setHeight(20);
         $drawing2->setCoordinates('F14');
 
