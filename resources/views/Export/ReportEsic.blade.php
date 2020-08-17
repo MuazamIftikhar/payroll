8a44d32617915ef5622551a751c21a73e770c4bf
MuazamIftikhar <51464624+MuazamIftikhar@users.noreply.github.com>
2020-08-17 13:39:21 +0500

    Changes excel

--- resources/views/Export/ReportEsic.blade.php
+++ resources/views/Export/ReportEsic.blade.php
@@ -7,9 +7,9 @@
         <tr>
             <td colspan="1" rowspan="3">IP Number </td>
             <td colspan="1" rowspan="3">IP Name </td>
-            <td colspan="2" rowspan="3">No of Days for which wages paid/payable during the month </td>
+            <td colspan="1" rowspan="3">No. of Days </td>
             <td colspan="2" rowspan="3">Total Monthly Wages</td>
-            <td colspan="2" rowspan="3"> Reason Code for Zero workings days(numeric only; provide 0 for all other reasons- Click on the link for reference)</td>
+            <td colspan="3" rowspan="3"> Reason Code for Zero workings days(numeric only; provide 0 for all other reasons- Click on the link for reference)</td>
             <td colspan="1" rowspan="3"> Last Working Day</td>
         </tr>
         <tr></tr>
@@ -20,16 +20,16 @@
             @endphp
         <tr>
             <td colspan="1">{{$e->esicNumber}}</td>
-            <td colspan="1">{{$e->salary_flag}} </td>
-            <td colspan="2">{{$e->PR_Day}} </td>
+            <td colspan="1">{{$e->Name}} </td>
+            <td colspan="1">{{$e->Total}} </td>
             @foreach(json_decode($e->salary_head) as $s)
                 @php
                     $sum=$sum+$s;
                 @endphp
             @endforeach
             <td colspan="2">{{$sum}}</td>
-            <td colspan="2"> </td>
-            <td colspan="1"> {{$e->DOE}}</td>
+            <td colspan="3"> </td>
+            <td colspan="1"></td>
         </tr>
             @endforeach
         </tbody>
