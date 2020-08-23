88ffa6f0b07b052ba4fd95166a462fe19b7619d1
MuazamIftikhar <51464624+MuazamIftikhar@users.noreply.github.com>
2020-08-23 16:01:54 +0500

    Changes excel

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -150,7 +150,7 @@ class UserController extends Controller
         $row=Attendance::select(DB::raw('SUM(Total) AS Total,SUM(assignDay) as a_today'))->where('employee_id','=',$id)
             ->where('Month','>=',$fromMonth)
             ->where('Month','<=',$toMonth)->groupBy('employee_id')->get();
-
+dd($salary_flag);
         if (count($row) > 0) {
             $total = $row[0]->Total;
             $assignDay = $row[0]->a_today;
